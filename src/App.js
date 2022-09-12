00159edf4d24a930404d7f8828b244ae64dadabc
Sabina <Sabinagli45@gmail.com>
2022-09-12 11:11:47 +0400

    add: list logic, pagemarkup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import React from "react";
+import TodoList from "./components/TodoList";
+
 function App() {
-  return <div></div>;
+  const todos = [
+    { id: 1, completed: false, title: "Боль в зубе" },
+    { id: 2, completed: false, title: "Боль в голове" },
+    { id: 3, completed: false, title: "Боль в ноге" },
+  ];
+  return (
+    <div className="wrapper">
+      <h1>React</h1>
+      <TodoList todos={todos} />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/TodoItem.js
@@ -0,0 +1,18 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function TodoItem({ todo, index }) {
+  return (
+    <li>
+      {index + 1 + " "}
+      {todo.title}
+    </li>
+  );
+}
+
+TodoItem.propeTypes = {
+  todo: PropTypes.object.isRequired,
+  index: PropTypes.number,
+};
+
+export default TodoItem;
