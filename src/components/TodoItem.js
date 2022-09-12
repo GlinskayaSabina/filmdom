c394a0140cf0a8facaaaece8f9199b5fd8cfc241
Sabina <Sabinagli45@gmail.com>
2022-09-12 14:36:09 +0400

    add: elements style

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -4,8 +4,10 @@ import PropTypes from "prop-types";
 function TodoItem({ todo, index }) {
   return (
     <li>
+      <input type="checkbox" />
       {index + 1 + " "}
       {todo.title}
+      <button>&times;</button>
     </li>
   );
 }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,17 +2,9 @@ import React from "react";
 import TodoItem from "./TodoItem";
 import PropTypes from "prop-types";
 
-const styles = {
-  ul: {
-    listStyle: "none",
-    margin: 0,
-    padding: 0,
-  },
-};
-
 function TodoList(props) {
   return (
-    <ul style={styles.ul}>
+    <ul>
       {props.todos.map((todo, index) => {
         return <TodoItem todo={todo} key={todo.id} index={index} />;
       })}
