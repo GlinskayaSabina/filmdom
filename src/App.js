d8d563464ebdc024ac2f4c3495e99f095db304be
Sabina <Sabinagli45@gmail.com>
2022-09-13 12:12:25 +0400

    fix: todo status

--- src/App.js
+++ src/App.js
@@ -2,19 +2,21 @@ import React from "react";
 import TodoList from "./components/TodoList";
 
 function App() {
-  let todos = [
+  const [todos, setTodos] = React.useState([
     { id: 1, completed: false, title: "Боль в зубе" },
     { id: 2, completed: false, title: "Боль в голове" },
     { id: 3, completed: false, title: "Боль в ноге" },
-  ];
+  ]);
 
   function toggleTodo(id) {
-    todos = todos.map((todo) => {
-      if (todo.id === id) {
-        todo.completed = !todo.completed;
-      }
-      return todo;
-    });
+    setTodos(
+      todos.map((todo) => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      })
+    );
   }
 
   return (
